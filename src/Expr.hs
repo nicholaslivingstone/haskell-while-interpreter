5f7bcbcaceb4d7a20d7abbb0c1c53deef4691ba7
Nicholas Livingstone <nlivingsto@unm.edu>
2024-11-29 19:25:38 -0700

    Added basic expression type

---
+++ src/Expr.hs
@@ -0,0 +1,22 @@
+module Expr where
+
+data ArithExpr
+  = Num Int
+  | Var String
+  | Add ArithExpr ArithExpr
+  | Sub ArithExpr ArithExpr
+  | Mul ArithExpr ArithExpr
+  | Neg ArithExpr 
+  deriving (Show, Eq)
+
+data BoolExpr
+  = BoolLiteral Bool
+  | Not BoolExpr
+  | And BoolExpr BoolExpr
+  | Or BoolExpr BoolExpr
+  | Eq ArithExpr ArithExpr
+  | Lt ArithExpr ArithExpr
+  | Gt ArithExpr ArithExpr
+  | Leq ArithExpr ArithExpr
+  | Gtq ArithExpr ArithExpr
+  deriving (Show, Eq)
