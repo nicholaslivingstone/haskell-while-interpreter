1fc4243f7dc4a6c144bc91739682a755f25be249
Nicholas Livingstone <nlivingsto@unm.edu>
2024-11-30 22:54:42 -0700

    Created untested version of bool expression interpreter

--- src/Interpreter/BoolInterp.hs
+++ src/Interpreter/BoolInterp.hs
@@ -1,2 +1,37 @@
 module Interpreter.BoolInterp where
 
+import WhileLanguage (BoolExpr(..), ArithExpr(..))
+import Interpreter.WhileInterpreter (VarMap, WhileError(..))
+import Interpreter.ArithmeticInterp (interpArith)
+import Control.Monad.State
+
+type BoolResult = Either WhileError Bool
+
+interpBool :: BoolExpr -> State VarMap BoolResult
+interpBool (BoolLiteral b) = return (Right b)
+interpBool (Not b) = interpUnaryOp not b
+interpBool (And b1 b2) = interpBinOp (&&) b1 b2
+interpBool (Or b1 b2) = interpBinOp (||) b1 b2
+interpBool (Eq a1 a2) = interpBinArithOp (==) a1 a2
+interpBool (Lt a1 a2) = interpBinArithOp (<) a1 a2
+interpBool (Gt a1 a2) = interpBinArithOp (>) a1 a2
+interpBool (Leq a1 a2) = interpBinArithOp (<=) a1 a2
+interpBool (Geq a1 a2) = interpBinArithOp (>=) a1 a2
+
+interpBinOp :: (Bool -> Bool -> Bool) -> BoolExpr -> BoolExpr -> State VarMap BoolResult
+interpBinOp op b1 b2 = do
+  r1 <- interpBool b1
+  r2 <- interpBool b2
+  return $ (op <$> r1) <*> r2
+
+interpBinArithOp :: (Int -> Int -> Bool) -> ArithExpr -> ArithExpr -> State VarMap BoolResult
+interpBinArithOp op a1 a2 = do
+  r1 <- interpArith a1
+  r2 <- interpArith a2
+  return $ (op <$> r1) <*> r2
+
+interpUnaryOp :: (Bool -> Bool) -> BoolExpr -> State VarMap BoolResult
+interpUnaryOp op b = do
+  r <- interpBool b
+  return $ fmap op r
+
