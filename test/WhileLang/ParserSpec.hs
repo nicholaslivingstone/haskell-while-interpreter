0361b6f268fa3ae8bd0a9d7eecb8466d595cb645
Nicholas Livingstone <nlivingsto@unm.edu>
2024-12-11 01:36:47 -0700

    Implemented boolean parsing tests

--- test/WhileLang/ParserSpec.hs
+++ test/WhileLang/ParserSpec.hs
@@ -11,23 +11,29 @@ import Control.Monad.State
 
 import qualified Data.Map as Map
 import Test.QuickCheck.State (State(expected))
-import GHC.IO.Handle (NewlineMode(inputNL))
 
 expressNum :: Int -> ArithExpr
-expressNum n = if n < 0 then Neg (Num (abs n)) else Num n 
+expressNum n = if n < 0 then Neg (Num (abs n)) else Num n
 
-runArithParseTest :: ArithExpr -> String -> Expectation
-runArithParseTest expected input = do
-  let result = runParser pArithExpr "" (pack input)
+runGeneralParseText :: (Show a, Eq a) => Parser a -> a -> String -> Expectation
+runGeneralParseText parser expected input = do
+  let result = runParser parser "" (pack input)
   result `shouldBe` Right expected
+
+runArithParseTest = runGeneralParseText pArithExpr
+runBoolParseTest = runGeneralParseText pBoolExpr
+
+boolToWhileText :: Bool -> String
+boolToWhileText True = "true"
+boolToWhileText False = "false"
 
 spec :: Spec
 spec = do
-  describe "Parser" $ do
+  describe "Arithmetic Parsing" $ do
     it "Variable" $ do
       let varExpr = Var "x"
           varText = "x"
-          res = runParser pArithExpr "" varText 
+          res = runParser pArithExpr "" varText
       res `shouldBe` Right varExpr
     it "Parses random integer" $ property $ \n -> ioProperty $ runArithParseTest
       (expressNum n)
@@ -38,7 +44,7 @@ spec = do
     it "Subtraction" $ property $ \x y -> ioProperty $ runArithParseTest
       (Sub (expressNum x) (expressNum y))
       (show x ++ " - " ++ show y)
-    it "Multiplication" $ property $ \x y -> ioProperty $ runArithParseTest 
+    it "Multiplication" $ property $ \x y -> ioProperty $ runArithParseTest
       (Mul (expressNum x) (expressNum y))
       (show x ++ "*" ++ show y)
     it "Division" $ property $ \x y -> ioProperty $ runArithParseTest
@@ -53,3 +59,43 @@ spec = do
     it "Parentheses" $ property $ \x y z -> ioProperty $ runArithParseTest
       (Add (Mul (expressNum x) (expressNum y)) (expressNum z))
       (show x ++ "*" ++ show y ++ "+" ++ show z)
+  describe "Boolean Parsing" $ do
+    it "Boolean Literals" $ property $ \b -> ioProperty $ runBoolParseTest
+      (BoolLiteral b)
+      (boolToWhileText b)
+
+    it "Equality" $ property $ \x y -> ioProperty $ runBoolParseTest
+      (Eq (expressNum x) (expressNum y))
+      (show x ++ " == " ++ show y)
+
+    it "Less Than" $ property $ \x y -> ioProperty $ runBoolParseTest
+      (Lt (expressNum x) (expressNum y))
+      (show x ++ " < " ++ show y)
+
+    it "Greater Than" $ property $ \x y -> ioProperty $ runBoolParseTest
+      (Gt (expressNum x) (expressNum y))
+      (show x ++ " > " ++ show y)
+
+    it "Less Than or Equal" $ property $ \x y -> ioProperty $ runBoolParseTest
+      (Leq (expressNum x) (expressNum y))
+      (show x ++ " <= " ++ show y)
+
+    it "Greater Than or Equal" $ property $ \x y -> ioProperty $ runBoolParseTest
+      (Geq (expressNum x) (expressNum y))
+      (show x ++ " >= " ++ show y)
+
+    it "Negation" $ property $ \b -> ioProperty $ runBoolParseTest
+      (Not (BoolLiteral b))
+      ("not " ++ boolToWhileText b)
+
+    it "Logical AND" $ property $ \b1 b2 -> ioProperty $ runBoolParseTest
+      (And (BoolLiteral b1) (BoolLiteral b2))
+      (boolToWhileText b1 ++ " and " ++ boolToWhileText b2)
+
+    it "Logical OR" $ property $ \b1 b2 -> ioProperty $ runBoolParseTest
+      (Or (BoolLiteral b1) (BoolLiteral b2))
+      (boolToWhileText b1 ++ " or " ++ boolToWhileText b2)
+
+    it "Combined Logical Expressions" $ property $ \b1 b2 -> ioProperty $ runBoolParseTest
+      (Or (And (BoolLiteral b1) (BoolLiteral b2)) (Not (BoolLiteral b1)))
+      ("[" ++ boolToWhileText b1 ++ " and " ++ boolToWhileText b2 ++ "] or not " ++ boolToWhileText b1)
