504b7aeb23f7b0369ebd3a3a32aeb1f0f7c88d14
Nicholas Livingstone <nlivingsto@unm.edu>
2024-12-11 02:25:56 -0700

    Implemented command parsing tests

--- test/WhileLang/ParserSpec.hs
+++ test/WhileLang/ParserSpec.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module WhileLang.ParserSpec (spec) where
 
-import Data.Text
+import Data.Text hiding (null)
 import Text.Megaparsec
 import Test.Hspec
 import Test.QuickCheck
@@ -22,10 +22,17 @@ runGeneralParseTest parser expected input = do
 
 runArithParseTest = runGeneralParseTest pArithExpr
 runBoolParseTest = runGeneralParseTest pBoolExpr
+runCommandParseTest = runGeneralParseTest pCommand
 
 boolToWhileText :: Bool -> String
 boolToWhileText True = "true"
 boolToWhileText False = "false"
+
+nonEmptyVarName :: Gen String
+nonEmptyVarName = do
+  firstChar <- elements (['a'..'z'] ++ ['A'..'Z'])  -- First character must be a letter
+  rest <- listOf (elements (['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']))  -- Rest can include letters and digits
+  return (firstChar : rest)
 
 spec :: Spec
 spec = do
@@ -99,3 +106,26 @@ spec = do
     it "Combined Logical Expressions" $ property $ \b1 b2 -> ioProperty $ runBoolParseTest
       (Or (And (BoolLiteral b1) (BoolLiteral b2)) (Not (BoolLiteral b1)))
       ("[" ++ boolToWhileText b1 ++ " and " ++ boolToWhileText b2 ++ "] or not " ++ boolToWhileText b1)
+  describe "Command Parsing" $ do
+    it "Parses variable assignment" $ property $ forAll nonEmptyVarName $ \x n -> ioProperty $ runCommandParseTest
+      (Assign x (expressNum n))
+      (x ++ " := " ++ show n)
+
+    it "Parses skip command" $ do
+      runCommandParseTest Skip "skip"
+
+    it "Parses if-then-else command" $ property $ \b n1 n2 -> ioProperty $ runCommandParseTest
+      (If (BoolLiteral b) (Assign "x" (expressNum n1)) (Assign "x" (expressNum n2)))
+      ("if " ++ boolToWhileText b ++ " then x := " ++ show n1 ++ " else x := " ++ show n2 ++ " fi")
+
+    it "Parses while command" $ property $ \b n -> ioProperty $ runCommandParseTest
+      (While (BoolLiteral b) (Assign "x" (expressNum n)))
+      ("while " ++ boolToWhileText b ++ " do x := " ++ show n ++ " od")
+
+    it "Parses sequential commands" $ property $ \n1 n2 -> ioProperty $ runCommandParseTest
+      (Seq [Assign "x" (expressNum n1), Assign "y" (expressNum n2)])
+      ("x := " ++ show n1 ++ "; y := " ++ show n2)
+
+    it "Parses print command" $ do
+      runCommandParseTest (Print "x") "putvarln x"
+
